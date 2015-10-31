cb0a1e365eb38f2b57f7da80cc3379ade191a008
EC2 Default User <ec2-user@ip-172-31-61-93.ec2.internal>
2015-10-31 15:14:41 +0000

    dont print zeros

--- main.cu
+++ main.cu
@@ -2,20 +2,50 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #define BUFLEN (4096)
 
-int
+int byte_sum(uint8_t* bytes, int size)
+{
+    int i = 0;
+    int sum = 0;
+    for(; i< size; i++)
+    {
+        sum += bytes[i];
+    }
+    return sum;
+}
+void print_nonzeros(uint8_t* bytes, int size)
+{
+    int i = 0;
+    for(; i< size; i++)
+    {
+        if (bytes[i] != 0)
+            printf("%x", bytes[i]);
+    }
+    return;
+}
+    int
 main(int argc, char *argv[])
 {
-	unsigned char host_buf[BUFLEN];
-	unsigned char *gpu_buf;
+    unsigned char host_buf[BUFLEN];
+    unsigned char *gpu_buf;
+    uint64_t num_bytes =0;
 
-	while (cudaMalloc(&gpu_buf, BUFLEN) == cudaSuccess) {
-		if (cudaMemcpy(host_buf, gpu_buf, BUFLEN, cudaMemcpyDeviceToHost))
-			err(1, "cudaMemcpy failed\n");
-		write(STDOUT_FILENO, host_buf, BUFLEN);
-	}
+    while (cudaMalloc(&gpu_buf, BUFLEN) == cudaSuccess) {
+        num_bytes+=BUFLEN;
+        if (cudaMemcpy(host_buf, gpu_buf, BUFLEN, cudaMemcpyDeviceToHost))
+            err(1, "cudaMemcpy failed\n");
+    
+        if (byte_sum(host_buf, BUFLEN) < 768)
+        {
+            continue;
+        }
+        write(fileno(stdout), host_buf, BUFLEN);
+        printf("\n");
+    }
+    printf("%lld bytes copied\n",num_bytes);
 
-	return 0;
+    return 0;
 }
